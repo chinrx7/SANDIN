889f3eaf2cf6b819e8be9f153bffe75a7de035c3
chinrx7 <34773359+chinrx7@users.noreply.github.com>
2018-02-22 12:07:52 +0700

    fix

--- read.php
+++ read.php
@@ -1,7 +1,8 @@
 <?php
 $txt = file_get_contents('http://735b07e66e5c.sn.mynetname.net:8080/flow.txt');
 $flow = explode(",",$txt);
-echo "<br>flow1 " + $flow[0];
-echo "<br>flow2 " + $flow[1];
-echo "<br>flow3 " + $flow[2];
+echo "flow1";
+echo $flow[0];
+echo $flow[1];
+echo $flow[2];
 ?>
