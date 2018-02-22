c20027bf498aca74e1423c1e2316fe6c3c246f2b
chinrx7 <34773359+chinrx7@users.noreply.github.com>
2018-02-22 13:41:50 +0700

    die

--- read.php
+++ read.php
@@ -1,5 +1,5 @@
 <?php
-$txt = file_get_contents('http://735b07e66e5c.sn.mynetname.net:8080/flow.txt');
+$txt = file_get_contents('http://735b07e66e5c.sn.mynetname.net:8080/flow.txt') or die echo("cannote read");
 
 echo "flow1";
 echo $txt;
