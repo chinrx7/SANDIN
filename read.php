5514034133b6e168cb135569e40db639181e6f76
chinrx7 <34773359+chinrx7@users.noreply.github.com>
2018-02-22 11:29:34 +0700

    fix read

--- read.php
+++ read.php
@@ -1,5 +1,4 @@
 <?php
-$flowfile = fopen("http://735b07e66e5c.sn.mynetname.net:8080/flow.txt","r") or die("Unable to open file");
-echo fge($flowfile);
-fclose($flowfile);
+$txt = file_get_contents('http://735b07e66e5c.sn.mynetname.net:8080/flow.txt');
+echo $txt;
 ?>
