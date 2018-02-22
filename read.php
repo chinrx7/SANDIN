b67311521bcdd6b87d500805c50798adfc0211bd
chinrx7 <34773359+chinrx7@users.noreply.github.com>
2018-02-22 11:46:53 +0700

    fix new line

--- read.php
+++ read.php
@@ -1,7 +1,7 @@
 <?php
 $txt = file_get_contents('http://735b07e66e5c.sn.mynetname.net:8080/flow.txt');
 $flow = explode(",",$txt);
-echo "flow1 " + $flow[0] + "<br>";
-echo "flow2 " + $flow[1] + "<br>";
-echo "flow3 " + $flow[2] + "<br>";
+echo "flow1 " + $flow[0] + "\r\n";
+echo "flow2 " + $flow[1] + "\r\n";
+echo "flow3 " + $flow[2] + "\r\n";
 ?>
