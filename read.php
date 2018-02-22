0725ca70e9a3e27eef0a0bef1edafc1203b08abd
chinrx7 <34773359+chinrx7@users.noreply.github.com>
2018-02-22 12:06:17 +0700

    br

--- read.php
+++ read.php
@@ -1,7 +1,7 @@
 <?php
 $txt = file_get_contents('http://735b07e66e5c.sn.mynetname.net:8080/flow.txt');
 $flow = explode(",",$txt);
-echo "flow1 " + $flow[0] + " ";
-echo "flow2 " + $flow[1] + " ";
-echo "flow3 " + $flow[2] + " ";
+echo "<br>flow1 " + $flow[0];
+echo "<br>flow2 " + $flow[1];
+echo "<br>flow3 " + $flow[2];
 ?>
