01c023dd7ed98bb315f3fc8103350412fba29aa6
chinrx7 <34773359+chinrx7@users.noreply.github.com>
2018-05-25 14:42:41 +0700

    Revert "fix"
    This reverts commit 23aeea09f6380aa6c90dc2624b1ee7e1c806418d.

--- read.php
+++ read.php
@@ -2,5 +2,7 @@
 $txt = file_get_contents('http://735b07e66e5c.sn.mynetname.net:8080/flow.txt') or die echo("cannote read");
 
 echo "flow1";
-echo $txt;
+$txtspt = explode(",",$txt);
+echo $txtspt[0];
+echo "flow2";
 ?>
